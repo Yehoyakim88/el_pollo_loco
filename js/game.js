589141b33c7bb35fc31f549af8167710a21840b1
Yehoyakim88 <yehoyakim@mail.de>
2023-03-01 08:14:38 +0100

    adding feature of not displaying touch controls in desktop mode

--- js/game.js
+++ js/game.js
@@ -92,3 +92,18 @@ function checkScreen() {
       }
     }, 10)
 }
+
+function checkForMobileDevice() {
+    let touchControls = document.getElementById('touch-controls');
+    if (
+        navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
+      ) {
+        touchControls.classList.remove('d-none');
+      }
+}
